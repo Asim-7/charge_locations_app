7d7e8b9f47c41c1e2f5df3f2c1cd004b027e6ea2
asim.shehzad <asim.shehzad@tomtom.com>
2025-08-18 12:51:46 +0200

    Custom app theme created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:charge_locations_app/di/service_locator.dart';
 import 'package:charge_locations_app/navigation/app_router.dart';
+import 'package:charge_locations_app/presentation/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,9 @@ class ChargeLocationsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Charge Locations',
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: AppTheme.lightTheme, // Light theme
+      darkTheme: AppTheme.darkTheme, // Dark theme
+      themeMode: ThemeMode.system, // Auto switch based on device setting
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.generateRoute,
       initialRoute: AppRouter.searchScreenRoute,
