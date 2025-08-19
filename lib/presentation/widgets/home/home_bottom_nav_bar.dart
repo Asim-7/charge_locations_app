1d1edc400d1c1ecc35a15f5d4f44838fbd971c77
asim.shehzad <asim.shehzad@tomtom.com>
2025-08-19 16:15:32 +0200

    remembering click state

--- lib/presentation/widgets/home/home_bottom_nav_bar.dart
+++ lib/presentation/widgets/home/home_bottom_nav_bar.dart
@@ -1,52 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:charge_locations_app/presentation/bloc/bottom_nav_cubit.dart';
 
 class HomeBottomNavBar extends StatelessWidget {
   final Color accentGreen;
   final void Function(int) onItemTapped;
   const HomeBottomNavBar({
+    super.key,
     required this.accentGreen,
     required this.onItemTapped,
-    super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(26),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      margin: const EdgeInsets.only(bottom: 30, left: 20, right: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-            icon: Icon(Icons.home_rounded, color: accentGreen, size: 28),
-            onPressed: () => onItemTapped(0),
+    return BlocBuilder<BottomNavCubit, int>(
+      builder: (context, selectedIndex) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(26),
           ),
-          IconButton(
-            icon: const Icon(
-              Icons.ev_station_outlined,
-              color: Colors.grey,
-              size: 28,
-            ),
-            onPressed: () => onItemTapped(1),
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          margin: const EdgeInsets.only(bottom: 30, left: 20, right: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.home_rounded,
+                  color: selectedIndex == 0 ? accentGreen : Colors.grey,
+                  size: 28,
+                ),
+                onPressed: () => onItemTapped(0),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.ev_station_outlined,
+                  color: selectedIndex == 1 ? accentGreen : Colors.grey,
+                  size: 28,
+                ),
+                onPressed: () => onItemTapped(1),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.map_outlined,
+                  color: selectedIndex == 2 ? accentGreen : Colors.grey,
+                  size: 28,
+                ),
+                onPressed: () => onItemTapped(2),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.person_outline,
+                  color: selectedIndex == 3 ? accentGreen : Colors.grey,
+                  size: 28,
+                ),
+                onPressed: () => onItemTapped(3),
+              ),
+            ],
           ),
-          IconButton(
-            icon: const Icon(Icons.map_outlined, color: Colors.grey, size: 28),
-            onPressed: () => onItemTapped(2),
-          ),
-          IconButton(
-            icon: const Icon(
-              Icons.person_outline,
-              color: Colors.grey,
-              size: 28,
-            ),
-            onPressed: () => onItemTapped(3),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
